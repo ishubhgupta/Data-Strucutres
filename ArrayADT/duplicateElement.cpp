8c305c1427b7d490c53f58b49240a49411c0db57
Shubh Gupta <shubhorai12@gmail.com>
2023-11-29 22:40:41 +0530

    duplicates

--- ArrayADT/duplicateElement.cpp
+++ ArrayADT/duplicateElement.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
 using namespace std;
+// sorted Array
 
-int main(){
-    int arr1[] = {3,6,6,8,8,8,9,12,15,15,15,16,20,20};
-    int l = 3;
-    int h = 20;
+void method1(){
+    int arr1[] = {3, 6, 6, 8, 8, 8, 9, 12, 15, 15, 15, 16, 20, 20};
+    int n = 14;
+    int lastDuplicate = 0;
+    for(int i=0;i<n-1;i++){
+        if(arr1[i]==arr1[i+1]){
+            int j = i+1;
+            while(arr1[j]==arr1[i]){
+                j++;
+            }
+            cout<<arr1[i]<<" is repeating "<<j-i<<" times."<<endl;
+        }
+    }
+}
 
-    int H[h + 1]; // Adding 1 to the size to accommodate elements from 0 to h
-    for(int i = 0; i <= h; i++){
+void hashingMethod() {
+    int arr1[] = {3, 6, 6, 8, 8, 8, 9, 12, 15, 15, 15, 16, 20, 20};
+    int h = sizeof(arr1) / sizeof(arr1[0]);
+
+    int H[h];
+
+    for (int i = 0; i < h; i++) {
         H[i] = 0;
     }
 
-    for(int i = 0; i < h; i++){
+    for (int i = 0; i < h; i++) {  
         H[arr1[i]]++;
     }
 
-    for(int i = 0; i <= h; i++){
-        if(H[i] > 1) {
+    for (int i = 0; i <= h; i++) {
+        if(H[i]>1){
             cout << i << " appears " << H[i] << " times." << endl;
         }
+        
     }
+}
+
+int main() {
+    // hashingMethod();
+    method1();
+    return 0;
 }
