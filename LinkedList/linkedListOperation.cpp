c8e548f650b0d35adf8bb2042f4f80d15eddc2b7
Shubh Gupta <shubhorai12@gmail.com>
2023-12-03 03:11:22 +0530

    counting nodes in linked list

--- LinkedList/linkedListOperation.cpp
+++ LinkedList/linkedListOperation.cpp
@@ -10,49 +10,75 @@ void create(int arr[], int n) {
     int i;
     struct Node *t, *last;
 
-    head = new Node;  // Corrected: Update the global head
+    head = new Node;
     head->data = arr[0];
     head->next = NULL;
     last = head;
 
-    for (i = 1; i < n; i++) {  // Corrected: Start the loop from 1
-        t = new Node;  // Corrected: Remove Node * before t
+    for (i = 1; i < n; i++) {
+        t = new Node;
         t->data = arr[i];
         t->next = NULL;
         last->next = t;
         last = t;
     }
 }
-// itretive method
+
+// Iterative method to display linked list
 void displayMethod1(struct Node *newNode) {
     while (newNode != nullptr) {
-        cout << newNode->data <<" ";
+        cout << newNode->data << " ";
         newNode = newNode->next;
     }
 }
 
-// display linked list using recursion here(first we print then call function again)
-void displayMethod2(struct Node *newNode){
-    if(newNode->next != NULL){
-        cout<<newNode->data<<" ";
+// Recursive method to display linked list (first print, then call function again)
+void displayMethod2(struct Node *newNode) {
+    if (newNode->next != NULL) {
+        cout << newNode->data << " ";
         displayMethod2(newNode->next);
     }
 }
-// here me first call function then print
-void displayMethod3(struct Node *newNode){
-    if(newNode->next != NULL){
+
+// Recursive method to display linked list (first call function, then print)
+void displayMethod3(struct Node *newNode) {
+    if (newNode->next != NULL) {
         displayMethod3(newNode->next);
-        cout<<newNode->data<<" ";
-        
+        cout << newNode->data << " ";
     }
+}
+
+// Count the number of elements in the linked list
+int countMethod1(struct Node *newNode) {
+    int c = 0;
+    while (newNode != nullptr) {
+        c++;
+        newNode = newNode->next;
+    }
+    return c;
+}
+
+// count number of element in linkedlist using recursive function
+int countMethod2(struct Node *newNode){
+    if(newNode == 0) return 0;
+    return countMethod2(newNode->next)+1;
 }
 
 int main() {
     int a[] = {2, 6, 8, 4, 9, 4};
     create(a, 6);
+
+    cout << "Display (Method-1) : ";
     displayMethod1(head);
-    cout<<endl<<"Display (Method-2) : ";displayMethod2(head);
-    cout<<endl<<"Display (Method-3) : ";displayMethod3(head);
+
+    cout << endl << "Display (Method-2) : ";
+    displayMethod2(head);
+
+    cout << endl << "Display (Method-3) : ";
+    displayMethod3(head);
+
+    cout << endl << "Count (Method-1) : " << countMethod1(head);
+    cout << endl << "Count (Method-2) : " << countMethod2(head);
 
     return 0;
 }
