2f8d0ca5baa2ca1d8e920255a59676695e31a138
Shubh Gupta <shubhorai12@gmail.com>
2024-05-12 19:46:48 +0530

    leetcode contest question

--- LinkedList/rev.cpp
+++ LinkedList/rev.cpp
@@ -1,24 +1,50 @@
-#include<iostream>
-#include<cstring>
-#include<stdlib.h>
+#include <iostream>
+#include <vector>
+#include <climits> // Include for INT_MIN
 using namespace std;
 
-int findPermutationDifference(string s, string t){
-    int n = s.length();
-    int sum=0;
-    for(int i =0; i<n;i++){
-        for(int j = 0; j<n;j++){
-            if(t[j] == s[i]){
-                sum += abs(i-j);
-                break;
-            }
+// int maximumEnergy(vector<int>& energy, int k) {
+//     int n = energy.size();
+//     int maxEnergy = INT_MIN;
+
+//     for (int i = 0; i < n; ++i) {
+//         int currentEnergy = 0;
+//         int j = i;
+//         while (j < n) {
+//             currentEnergy += energy[j];
+//             j += k;
+//         }
+//         maxEnergy = max(maxEnergy, currentEnergy);
+//     }
+
+//     return maxEnergy;
+    
+// }
+
+int maximumEnergy(vector<int>& energy, int k) {
+    int n = energy.size();
+    int maxEnergy = INT_MIN;
+    vector<int> vec;
+
+    for (int i = 0; i < n; ++i) {
+        int currentEnergy = 0;
+        int j = i;
+        while (j < n) {
+            currentEnergy += energy[j];
+            j += k;
         }
+        maxEnergy = max(maxEnergy, currentEnergy);
     }
-    return sum;
+
+    return maxEnergy;
+    
 }
 
-int main(){
-    string s = "abcde";
-    string t = "edbac";
-    cout<<findPermutationDifference(s, t);
+int main() {
+    vector<int> vec1 = {5, 2, -10, -5, 1};
+    cout << "Example 1: " << maximumEnergy(vec1, 3) << endl;
+    vector<int> vec2 = {-2, -3, -1};
+    cout << "Example 2: " << maximumEnergy(vec2, 2) << endl;
+
+    return 0;
 }
