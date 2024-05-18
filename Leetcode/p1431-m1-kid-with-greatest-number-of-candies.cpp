6bda582285cf447f138e65639fd21c1bde4babab
Shubh Gupta <shubhorai12@gmail.com>
2024-05-18 21:52:32 +0530

    solved problem p1431 kid with greatest number of candies

---
+++ Leetcode/p1431-m1-kid-with-greatest-number-of-candies.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    vector<bool> result;
+    int n = candies.size();
+    int max = *max_element(candies.begin(), candies.end());
+    for(int i = 0; i<n;i++){
+        int tc = candies[i] + extraCandies;
+        if(tc >= max) result.push_back(true);
+            else{
+                result.push_back(false);
+            }
+    }
+    return result;
+}
+
+int main(){
+    vector<int> vec1={2, 3, 5, 1, 3};
+    vector<bool> res;
+    res = kidsWithCandies(vec1, 4);
+    for(auto x: res){
+        cout<<x<<" ";
+    }
+}
