e059130bad7a1366bc509b10098b558b0ffabd53
Shubh Gupta <shubhorai12@gmail.com>
2024-05-23 21:22:13 +0530

    solved problem number of arthmetic triplets of leetcode

---
+++ Leetcode/p2367-m1-number-of-arthmetic-triplets.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int arithmeticTriplets(vector<int>& nums, int diff) {
+    int n = nums.size();
+    int count=0;
+    for(int i=0;i<n;i++){
+        for(int j = i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                if(nums[j]-nums[i] == diff && nums[k] - nums[j] == diff){
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+int main(){
+    vector<int> vec1 = {1, 2,3 , 4, 5, 6};
+    cout<<arithmeticTriplets(vec1, 3);
+    return 0;
+}
