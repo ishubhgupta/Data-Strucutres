73cc121e4ad7e144adc85fc8a32389bd6b8e40c9
Shubh Gupta <shubhorai12@gmail.com>
2024-05-25 20:48:59 +0530

    add another testcase in problem 2367 of leetcode

--- Leetcode/p2367-m1-number-of-arthmetic-triplets.cpp
+++ Leetcode/p2367-m1-number-of-arthmetic-triplets.cpp
@@ -21,6 +21,7 @@ int main(){
     vector<int> vec1 = {1, 2,3 , 4, 5, 6};
     cout<<arithmeticTriplets(vec1, 3);
     cout<<arithmeticTriplets(vec1, 8);
+    cout<<arithmeticTriplets(vec1, 5);
 
     return 0;
 }
