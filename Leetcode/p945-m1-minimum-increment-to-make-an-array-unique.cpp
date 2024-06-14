0444350b2475e36197c8ee4c5a132cfd2f216d0d
Shubh Gupta <shubhorai12@gmail.com>
2024-06-14 16:37:12 +0530

    solved problem p945 minimum increment to make an array unique

---
+++ Leetcode/p945-m1-minimum-increment-to-make-an-array-unique.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int minIncrementForUnique(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int count = 0;
+    int cmax = 0;
+    for(auto x:nums){
+        cmax = max(cmax, x);
+        count += cmax-x;
+        cmax+=1;
+    }
+    return count;
+}
+
+int main(){
+    vector<int> vec1 = {3,2,1,2,1,7};
+
+    cout<<minIncrementForUnique(vec1);
+    return 0;
+}
+
