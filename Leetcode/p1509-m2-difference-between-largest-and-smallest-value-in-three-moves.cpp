4487db23dbedda7e564359207f7798f943e3a0af
Shubh Gupta <shubhorai12@gmail.com>
2024-07-03 19:15:17 +0530

    p1509 problem of leetcode but with another method- doest pass all test cases

---
+++ Leetcode/p1509-m2-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -0,0 +1,24 @@
+// this is not correct solution for all test cases
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int minDifference(vector<int>& nums) {
+    if (nums.size() <= 4) return 0;
+
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+
+
+    nums[n-1] = nums[n-2] = nums[n-3] = nums[0];
+
+    return max_element(nums.begin(), nums.end()) - min_element(nums.begin(), nums.end());
+}
+
+
+int main() {
+    vector<int> nums = {1, 5, 0, 10, 14};
+    cout<<minDifference(nums)<<endl;
+    return 0;
+}
