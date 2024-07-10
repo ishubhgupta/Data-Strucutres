5f87c9c38911c8edc6557fe82c269359f6663884
Shubh Gupta <shubhorai12@gmail.com>
2024-07-10 18:56:47 +0530

    solved problem 2375 consturct smallest number from DI string

---
+++ Leetcode/p2375-m1-construct-smallest-number-from-DI-string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+string smallestNumber(string pattern) {
+    int n = pattern.size();
+    string result;
+    stack<int> st;
+
+    for (int i = 0; i <= n; ++i) {
+        st.push(i + 1);
+        if (i == n || pattern[i] == 'I') {
+            while (!st.empty()) {
+                result += to_string(st.top());
+                st.pop();
+            }
+        }
+    }
+
+    return result;
+}
+
+int main(){
+    string pattern = "IIIDIDDD";
+    cout<<smallestNumber(pattern);
+    return 0;
+}
